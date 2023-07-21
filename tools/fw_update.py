c2e34580102a66aa67170a4b3fe4f9f348ee2c9e
palindromic sodium <129000615+palindsodi@users.noreply.github.com>
2023-07-21 07:33:36 -0700

    dsf defaults

--- tools/fw_update.py
+++ tools/fw_update.py
@@ -110,7 +110,6 @@ def main(ser, infile, debug):
     sigkey = ECC.import_key(f.read())
 
     h = SHA256.new(metadata + firmware)
-    verifier = DSS.new(sigkey, "fips-186-3")
 
     try:
         verifier.verify(h, signature)
