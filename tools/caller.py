9e4b27025f1ab9b1752e22f3d6964ecb168686cd
Ashley Vaughn <trevor.n.vaughn@gmail.com>
2023-07-26 13:27:30 -0700

    unify uarts, clean caller, remove unnecessary files from old insecure example

--- tools/caller.py
+++ tools/caller.py
@@ -1,16 +1,11 @@
 import subprocess
 import os
 
-def main():
+if __name__ == "__main__":
     current_directory = os.path.dirname(os.path.abspath(__file__))
 
     print("Calling emulator")
-    
-    subprocess.run(["python", "./bl_emulate.py"], cwd=current_directory)
-    while (1):
-        s = 1
-    
-    
 
-if __name__ == "__main__":
-    main()
+    subprocess.run(["python", "./bl_emulate.py"], cwd=current_directory)
+    while True:
+        pass
