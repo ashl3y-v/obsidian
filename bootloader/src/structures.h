36460730e4b25242e3d0a455b298dbf2ec12df96
absceptual <absceptual@gmail.com>
2023-07-27 16:02:17 +0000

    added sig to structure

--- bootloader/src/structures.h
+++ bootloader/src/structures.h
@@ -1,7 +1,10 @@
 #include <stdint.h>
 
+#define SIGNATURE_SIZE 32
+
 typedef struct _metadata
 {
+    uint8_t signature[SIGNATURE_SIZE];
     volatile uint16_t version;
     volatile uint16_t  size;
     volatile uint16_t message_size;
