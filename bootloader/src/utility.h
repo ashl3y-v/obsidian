696f97480a877f014ced33b4a071bfa234d427cb
absceptual <absceptual@gmail.com>
2023-07-27 22:36:28 +0000

    we love beaverssl!

--- bootloader/src/utility.h
+++ bootloader/src/utility.h
@@ -10,6 +10,8 @@
 #include "driverlib/interrupt.h"
 #include "driverlib/sysctl.h"
 
+#include "beaverssl.h"
+
 void uart_write_hex_bytes(uint8_t uart, uint8_t* start, uint32_t len);
 void uart_read_wrp(uint8_t uart, int blocking, int* read, uint8_t* out, size_t bytes);
 void uart_write_wrp(uint8_t uart, uint8_t* in, size_t bytes);
